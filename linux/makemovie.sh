3fc7befeaf70e37ef2a6ec2a8636b123683729b9
George Campbell <geocolumbus@gmail.com>
2017-07-05 09:42:12 -0400

    Remove linux folder. Not needed now that ffmpeg is on the pi.

--- linux/makemovie.sh
+++
@@ -1,4 +0,0 @@
-ROOT=/home/george
-DATE=`date +%Y-%m-%d`
-rm "$ROOT/timelapse/movies/$DATE.mov"
-/usr/local/bin/ffmpeg -r 25 -pattern_type glob -i "$ROOT/timelapse/unprocessed/*.jpg" -c:v libx264 "$ROOT/timelapse/movies/$DATE.mov"

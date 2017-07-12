e1ec6904c2935d26af39259356731664666ac334
George Campbell <geocolumbus@gmail.com>
2017-07-12 14:08:41 -0400

    Update makemovie.sh

--- makemovie.sh
+++ makemovie.sh
@@ -1,5 +1,6 @@
+# Takes files from the timelapse/done folder and stiches them into a movie in the movies folder. 
 ROOT=/home/pi
 FILENAME=`date +%Y-%m-%d`
 rm "$ROOT/camera/movies/$FILENAME.mov"
-/usr/local/bin/ffmpeg -r 25 -pattern_type glob -i "$ROOT/camera/timelapse/done/*.jpg" -c:v libx264 "$ROOT/camera/movies/$FILENAME.mov"
+/usr/local/bin/ffmpeg -r 25 -pattern_type glob -i "$ROOT/pi-timelapse-camera/timelapse/done/*.jpg" -c:v libx264 "$ROOT/pi-timelapse-camera/movies/$FILENAME.mov"
 

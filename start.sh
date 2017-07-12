8169ee9b7e4b10cc859b6804b802802acdaa8b55
George Campbell <geocolumbus@gmail.com>
2017-07-12 14:10:35 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ `ps -ax | grep '/home/pi/camera/cam.py' | grep -v 'sudo' | grep -v 'grep' | wc -l` = "1" ]; then
+if [ `ps -ax | grep '/home/pi/pi-timelapse-camera/cam.py' | grep -v 'sudo' | grep -v 'grep' | wc -l` = "1" ]; then
    echo "[ALREADY RUNNING]"
-   echo `ps -ax | grep '/home/pi/camera/cam.py' | grep -v grep`
+   echo `ps -ax | grep '/home/pi/pi-timelapse-camera/cam.py' | grep -v grep`
 else
    echo "[STARTING]"
-   /usr/bin/python3 /home/pi/camera/cam.py &
-   echo `ps -ax | grep '/home/pi/camera/cam.py' | grep -v grep`
+   /usr/bin/python3 /home/pi/pi-timelapse-camera/cam.py &
+   echo `ps -ax | grep '/home/pi/pi-timelapse-camera/cam.py' | grep -v grep`
 fi

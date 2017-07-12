fdfc8d0234cde001213e95e4cd58edb9a59dd6e3
George Campbell <geocolumbus@gmail.com>
2017-07-12 14:09:07 -0400

    Update isrunning.sh

--- isrunning.sh
+++ isrunning.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ `ps -ax | grep '/home/pi/camera/cam.py' | grep -v grep | grep -v sudo | wc -l` = "1" ]; then
+if [ `ps -ax | grep '/home/pi/pi-timelapse-camera/cam.py' | grep -v grep | grep -v sudo | wc -l` = "1" ]; then
    echo "[RUNNING]"
-   echo  `ps -ax | grep '/home/pi/camera/cam.py' | grep -v grep`
+   echo  `ps -ax | grep '/home/pi/pi-timelapse-camera/cam.py' | grep -v grep`
 else
    echo "[PROCESS NOT FOUND]"
 fi

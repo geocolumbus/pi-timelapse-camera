60b247e6fe99bdf23ca5e0dc2230adffff979737
George Campbell <geocolumbus@gmail.com>
2017-07-12 14:11:09 -0400

    Update timestamp.sh

--- timestamp.sh
+++ timestamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DONE_DIR=/home/pi/camera/timelapse/done
+DONE_DIR=/home/pi/pi-timelapse-camera/timelapse/done
 
 # only proceed if the done directory is empty
 # if find "$DONE_DIR" -mindepth 1 -print -quit | grep -q .
@@ -8,7 +8,7 @@ DONE_DIR=/home/pi/camera/timelapse/done
 #     echo "/done directory isn't empty - aborting."
 # else
     # loop through the files and move them to done
-    FILES=/home/pi/camera/timelapse/*.jpg
+    FILES=/home/pi/pi-timelapse-camera/timelapse/*.jpg
     for f in $FILES
     do
         /usr/bin/convert -pointsize 60 -fill yellow -draw "text 50,1020 \"`date '+%Y-%m-%d %H:%m'`\"" $f $f
